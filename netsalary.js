80a49ba630fc4fbf3f085317fec2e514492ac4b3
Reagan Mariera <162294868+Reagan-marera@users.noreply.github.com>
2024-03-24 21:30:03 +0300

    Update netsalary.js

--- netsalary.js
+++ netsalary.js
@@ -1,19 +1,36 @@
-function calculateNetSalary(basicSalary, benefits) {
-    const taxRate = 0.15;
-    const NHIFDeductionRate = 0.025;
-    const NSSFRate = 0.06;
-
-    const grossSalary = basicSalary + benefits;
-    const tax = grossSalary * taxRate;
-    const NHIFDeduction = grossSalary * NHIFRate;
-    const NSSFDeduction = grossSalary * NSSFfRate;
-
-    const netSalary = grossSalary - tax - NHIFDeductionDeduction - NSSFDeductionDeduction;
-    return netSalary;
+function show(value){
+    console.log(value);
 }
 
-const basicSalary = parseFloat(prompt("Enter basic salary: "));
-const benefits = parseFloat(prompt("Enter benefits: "));
 
-const netSalary = calculateNetSalary(basicSalary, benefits);
-console.log("Net Salary:", netSalary);
+let basicSalary = 600000;
+let benefit = 4200;
+
+
+function tax(taxableIncome){
+    let paye = 0;
+    let grossTax = 0;
+    let rated = 0; 
+    let personalRelief = 2400;
+
+    if(taxableIncome > 0 && taxableIncome <= 24000){
+        grossTax = (taxableIncome*0.10);
+    }else if (taxableIncome >= 24001 && taxableIncome <= 32333){
+        rated = (taxableIncome - 24000) * 0.25;
+        grossTax = 2400 + rated
+    }else if (taxableIncome >= 32334 && taxableIncome <= 500000){
+        rated = (taxableIncome - 32333) * 0.30;
+        grossTax = 2400 + 2083.25 + rated
+    }else if (taxableIncome >= 500001 && taxableIncome <= 800000){
+        rated = (taxableIncome - 500000) * 0.325;
+        grossTax = 2400 + 2083.25 + 140300.1 + rated  
+    }else if(taxableIncome > 800000){
+        rated = (taxableIncome - 800000) * 0.35;
+        grossTax = 2400 + 2083.25 + 140300.1 + 97500 + rated
+    }
+    paye =  ~~(grossTax - personalRelief);
+    netsalary = (taxableIncome - paye);
+  return { paye, netsalary } 
+}
+show(tax(75000))
+
